89a8c7d0c62e7235aaa71dfc04e937920d5661cd
모준서 <mojunseo49@naver.com>
2024-05-24 11:04:31 +0900

    Feature/test#7 (#48)
    * test: test log 추가
    * test: test log 추가 2222

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,10 +30,11 @@ dependencies {
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("io.github.microutils:kotlin-logging:1.12.5")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
-    
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
