32f8fcff12cc4571ad09c58d278893c0c0df9113
모준서 <mojunseo49@naver.com>
2024-05-28 09:24:14 +0900

    fix: OUTPUT 변수 수정 [stage, release] (#76)

--- src/main/kotlin/org/example/jenkinstesting/JenkinsTestingApplication.kt
+++ src/main/kotlin/org/example/jenkinstesting/JenkinsTestingApplication.kt
@@ -11,6 +11,9 @@ fun main(args: Array<String>) {
     val log = KotlinLogging.logger { }
     runApplication<JenkinsTestingApplication>(*args)
     log.info { "========= CHECk CHECK ========" }
+    log.info { "========= ROLLBACK TEST ========" }
+    log.info { "========= ROLLBACK TEST ========" }
+    log.info { "========= ROLLBACK TEST ========" }
     log.info { "========= CHECk CHECK ========" }
     log.info { "========= 967eefc0abab3a10817c30341c28f628ff584c52 ========" }
 }
