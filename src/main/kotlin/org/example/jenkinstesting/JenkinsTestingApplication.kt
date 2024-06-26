454acfa032279031e10a29090384a922a1594344
jayking <mojunseo49@naver.com>
2024-06-26 13:13:18 +0900

    test: test on feature task 2

--- src/main/kotlin/org/example/jenkinstesting/JenkinsTestingApplication.kt
+++ src/main/kotlin/org/example/jenkinstesting/JenkinsTestingApplication.kt
@@ -12,7 +12,6 @@ fun main(args: Array<String>) {
     runApplication<JenkinsTestingApplication>(*args)
     log.info { "========= CHECk CHECK ========" }
     log.info { "========= ROLLBACK TEST ========" }
-    log.info { "========= ROLLBACK TEST ========" }
     log.info { "========= CHECk CHECK ========" }
     log.info { "========= 967eefc0abab3a10817c30341c28f628ff584c52 ========" }
     log.info { "hotfix on qa branch" }
